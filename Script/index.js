11ff99a2d5fd76244e64c27e63cb4f035958a180
OnniPajarinen <onni.pajarinen@student.laurea.fi>
2024-11-28 05:15:34 +0200

    s

--- Script/index.js
+++ Script/index.js
@@ -1,18 +1,52 @@
 var lista = document.getElementsByTagName("li");
 var i;
+
 for (i = 0; i < lista.length; i++) {
     var kohta = document.createElement("span");
     var teksti = document.createTextNode("X");
-    button.className ="close";
-    button.appendChild(teksti);
-    lista[i].appendChild(button);
+    kohta.className = "close";
+    kohta.appendChild(teksti);
+    lista[i].appendChild(kohta);
 }
 
 var close = document.getElementsByClassName("close");
-var i;
 for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
-        var div= this.parentElement;
+        var div = this.parentElement;
         div.style.display = "none";
+    };
+}
+
+var list = document.querySelector("ul");
+list.addEventListener("click", function(event) {
+    if (event.target.tagName === "LI") {
+        event.target.classList.toggle("checked");
+    }
+}, false);
+
+function addNew() {
+    var li = document.createElement("li");
+    var muisti = document.getElementById("muisti").value;
+    var okei = document.createTextNode(muisti);
+    li.appendChild(okei);
+
+    if (muisti === "") {
+        alert("The field cannot be empty!");
+    } else {
+        document.getElementById("ul").appendChild(li);
+    }
+
+    var kohta = document.createElement("span");
+    var teksti = document.createTextNode("X");
+    kohta.className = "close";
+    kohta.appendChild(teksti);
+    li.appendChild(kohta);
+
+    var close = document.getElementsByClassName("close");
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        };
     }
 }
